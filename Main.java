ed6afa20352d86a2ed6de31990454935271be387
otherPOM <otherPOM@gmail.com>
2021-01-25 13:08:13 +0300

    Stage 2 done

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
 package readability;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        var line = scan.nextLine();
+        var sentences = line.split("[.!?]");
+        var avg = Arrays.stream(sentences)
+                .filter(Predicate.not(String::isBlank))
+                .mapToLong(Main::countWords)
+                .sum() / (double) sentences.length;
+        System.out.println(avg > 10 ? "HARD" : "EASY");
+    }
+
+    private static long countWords(String sentence) {
+        return Arrays.stream(sentence.split("\\s+"))
+                .filter(Predicate.not(String::isBlank))
+                .count();
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Stage 1
 Let's create a simple program. If the text contains more than 100 symbols (including spacebars and punctuation) then the text is considered hard to read. Else, the text is considered easy to read. If the text contains exactly 100 symbols then it is still easy to read.
 
+## Stage 2
+Let's suppose that if the text contains on average more than 10 words per sentence, then this text is hard to read. Otherwise, this text is easy to read.
+
