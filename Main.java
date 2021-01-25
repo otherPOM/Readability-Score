ea88716d1fa667be99b6cc838d48d089f4b6e03b
otherPOM <otherPOM@gmail.com>
2021-01-25 15:55:22 +0300

    Stage 3 done

--- Main.java
+++ Main.java
@@ -1,25 +1,68 @@
 package readability;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
-    private static final Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        var line = scan.nextLine();
-        var sentences = line.split("[.!?]");
-        var avg = Arrays.stream(sentences)
+    public static void main(String[] args) throws IOException {
+        var path = Path.of(args[0]);
+        var lines = Files.lines(path);
+        System.out.println("The text is:");
+        lines.forEach(System.out::println);
+        System.out.println();
+
+        var res = Files.lines(path)
+                .map(Main::process)
+                .reduce((x, y) -> {
+                    x.replaceAll((k, v) -> v + y.get(k));
+                    return x;
+                }).get();
+
+        System.out.println("Words: " + res.get("Words"));
+        System.out.println("Sentences: " + res.get("Sentences"));
+        System.out.println("Characters: " + res.get("Characters"));
+
+        var score = ari(res);
+        System.out.printf("The score is: %.2f\n", score);
+        System.out.println("This text should be understood by " + getAge(score) + "-year-olds.");
+    }
+
+    private static Map<String, Integer> process(String text) {
+        var unitToCount = new HashMap<String, Integer>();
+        unitToCount.put("Characters",
+                text.replaceAll("\\s+", "").length());
+        var sentences = Arrays.stream(text.split("[.!?]+"))
                 .filter(Predicate.not(String::isBlank))
-                .mapToLong(Main::countWords)
-                .sum() / (double) sentences.length;
-        System.out.println(avg > 10 ? "HARD" : "EASY");
+                .collect(Collectors.toList());
+        unitToCount.put("Sentences", sentences.size());
+        unitToCount.put("Words", sentences.stream().mapToInt(s -> (int) countWords(s)).sum());
+        return unitToCount;
     }
 
     private static long countWords(String sentence) {
         return Arrays.stream(sentence.split("\\s+"))
                 .filter(Predicate.not(String::isBlank))
                 .count();
+    }
+
+    private static double ari(Map<String, Integer> unitToCount) {
+        return 4.71
+                * (unitToCount.get("Characters") / (double) unitToCount.get("Words"))
+                + 0.5
+                * (unitToCount.get("Words") / (double) unitToCount.get("Sentences"))
+                - 21.43;
+    }
+
+    private static String getAge(double score) {
+        var ages = new String[]{"5-6", "6-7", "7-9", "9-10", "10-11", "11-12", "12-13",
+        "13-14", "14-15", "15-16", "16-17", "17-18", "18-24", "24+"};
+        return ages[(int) Math.ceil(score) - 1];
     }
 }
--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Let's create a simple program. If the text contains more than 100 symbols (inclu
 ## Stage 2
 Let's suppose that if the text contains on average more than 10 words per sentence, then this text is hard to read. Otherwise, this text is easy to read.
 
+## Stage 3
+In this stage, you will program the Automated readability index. It was introduced in 1968 and a lot of research works rely on this. 
+You can look at different ages corresponding to the different scores by the table in this  article.
+Also, your program should read a file instead of typing a text manually. You should pass the filename through the command line arguments.
+The program should output the score itself and an approximate age needed to comprehend the text.
+Use the appropriate rounding function to calculate the score as integer.
+You should also print how many characters, words, and sentences the text has.
+The number of characters is any visible symbol (so, in the real text it's everything except space, newline "\n" and tab "\t").
+Notice, that the text can contain multiple lines, not just a single line like in the previous stages. You should analyze all the lines.
+
